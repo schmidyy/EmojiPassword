21ede9f86e4a26684d018df30119b0cd7bee1c16
Mat Schmid <matschmidy@gmail.com>
2017-03-09 22:49:16 -0500

    Initial commit

---
+++ EmojiPassword/SecondViewController.swift
@@ -0,0 +1,81 @@
+//
+//  SecondViewController.swift
+//  EmojiPassword
+//
+//  Created by Mat Schmid on 2017-03-08.
+//  Copyright © 2017 Mat Schmid. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    @IBOutlet weak var firstSlot: UIImageView!
+    @IBOutlet weak var secondSlot: UIImageView!
+    @IBOutlet weak var thirdSlot: UIImageView!
+    @IBOutlet weak var fourthSlot: UIImageView!
+    
+    @IBOutlet weak var textLabel: UILabel!
+    
+    var slots : [UIImageView] = []
+    var currSlot: Int = 0
+    var pass : [UIImage] = []
+    var numFails: Int = 0
+    
+
+    
+    @IBAction func emojiClicked(_ sender: UIButton) {
+        if currSlot < 4 {
+            slots[currSlot].image = sender.currentImage
+            currSlot += 1
+        }
+    }
+    
+    @IBAction func backspace(_ sender: UIButton) {
+        if currSlot > 0 {
+            currSlot -= 1
+            slots[currSlot].image = nil
+        }
+    }
+    
+    func clear() {
+        for i in slots{
+            i.image = nil
+        }
+        currSlot = 0
+    }
+    
+    @IBAction func checkPassword(_ sender: UIButton) {
+        var count : Int = 0
+        if currSlot == 4 {
+            for i in 0..<4{
+                if pass[i] == slots[i].image{
+                    count += 1
+                }
+                else{
+                    textLabel.text = "Please try again"
+                    clear()
+                }
+            }
+            if count == 4 {
+                textLabel.text = "Correct!"
+                for i in slots{
+                    i.image = #imageLiteral(resourceName: "smile-glasses")
+                }
+            }
+        }
+        else {
+            self.numFails += 1
+            textLabel.text = "Place 4 emojis"
+        }
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        slots.append(firstSlot)
+        slots.append(secondSlot)
+        slots.append(thirdSlot)
+        slots.append(fourthSlot)
+    }
+}
--- EmojiPassword/ViewController.swift
+++ EmojiPassword/ViewController.swift
@@ -9,17 +9,98 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
+    @IBOutlet weak var first: UIImageView!
+    @IBOutlet weak var second: UIImageView!
+    @IBOutlet weak var third: UIImageView!
+    @IBOutlet weak var fourth: UIImageView!
+    
+    @IBOutlet weak var generateButton: UIButton!
+    @IBOutlet weak var nextButton: UIButton!
+    
+    var password : [UIImage] = []
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nextButton.isHidden = true
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func loadEmojis() -> [UIImage]{
+        var emojis : [UIImage] = []
+        emojis.append(UIImage(named: "smile-glasses")!)
+        emojis.append(UIImage(named: "tired")!)
+        emojis.append(UIImage(named: "hmm")!)
+        emojis.append(UIImage(named: "heart-eyes")!)
+        emojis.append(UIImage(named: "laughing")!)
+        emojis.append(UIImage(named: "smile-halo")!)
+        emojis.append(UIImage(named: "surprised")!)
+        emojis.append(UIImage(named: "tired")!)
+        emojis.append(UIImage(named: "tongue")!)
+        emojis.append(UIImage(named: "alien")!)
+        emojis.append(UIImage(named: "eggplant")!)
+        emojis.append(UIImage(named: "ghost")!)
+        emojis.append(UIImage(named: "heart")!)
+        emojis.append(UIImage(named: "hotdog")!)
+        emojis.append(UIImage(named: "monkey")!)
+        emojis.append(UIImage(named: "poop")!)
+        emojis.append(UIImage(named: "snake")!)
+        emojis.append(UIImage(named: "peach")!)
+        emojis.append(UIImage(named: "chilli")!)
+        emojis.append(UIImage(named: "sad")!)
+        emojis.append(UIImage(named: "uhoh")!)
+        emojis.append(UIImage(named: "dizzy")!)
+        emojis.append(UIImage(named: "neutral")!)
+        emojis.append(UIImage(named: "secret")!)
+        emojis.append(UIImage(named: "bomb")!)
+        emojis.append(UIImage(named: "fire")!)
+        emojis.append(UIImage(named: "sun")!)
+        emojis.append(UIImage(named: "peace")!)
+        emojis.append(UIImage(named: "money")!)
+        emojis.append(UIImage(named: "mad")!)
+        
+        return emojis
     }
+    
+    @IBAction func generate() {
+        var emojis = loadEmojis()
+
+        let indexFirst = Int(arc4random_uniform(UInt32(emojis.count)))
+        first.image = emojis[indexFirst]
+        
+        let indexSecond = Int(arc4random_uniform(UInt32(emojis.count)))
+        second.image = emojis[indexSecond]
+        
+        let indexThird = Int(arc4random_uniform(UInt32(emojis.count)))
+        third.image = emojis[indexThird]
+        
+        let indexFourth = Int(arc4random_uniform(UInt32(emojis.count)))
+        fourth.image = emojis[indexFourth]
+        
+        nextButton.isHidden = false
 
 
+    }
+    
+    @IBAction func nextScreen(_ sender: UIButton) {
+
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        self.password.append(first.image!)
+        self.password.append(second.image!)
+        self.password.append(third.image!)
+        self.password.append(fourth.image!)
+        
+        if segue.identifier == "goToVC"{
+            if let destination = segue.destination as? SecondViewController {
+                destination.pass = self.password
+            }
+        }
+    }
+    
+    
 }
 
